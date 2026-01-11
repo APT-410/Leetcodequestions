bb1a6df26a6a577fb1ff6a3d0181fbb32ca05d87
chris bukavich <cjbjames@gmail.com>
2026-01-11 01:35:35 -0800

    solved

---
+++ hackerrank/python-print.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    combinedstring = ""
+    for i in range(1,n+1,1):
+        combinedstring+=str(i)
+    print(combinedstring)
---
+++ hackerrank/python_loops.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    # we have to loop through this until we hit the end iterate one
+    for i in range(0,n,1):
+        print(i*i)
